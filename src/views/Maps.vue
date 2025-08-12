4b95aaf2f590e9d0ede3e1f6084964d4465b2d4b
Alex-Berridge <73975122+Alex-Berridge@users.noreply.github.com>
2025-08-12 16:19:03 +0100

    Fixed more things
    Altered gift wishlist text. Added paypal link. Fixed map on the maps page, Attempted to add preview to the invite but it's not worth the time

--- src/views/Maps.vue
+++ src/views/Maps.vue
@@ -1,6 +1,6 @@
 <template>
     <a href="https://maps.app.goo.gl/9QgQSayWAG9QSB6N7"><h1>The Lawn Club</h1></a>
-    <iframe 
+    <!-- <iframe 
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2374.383522124856!2d-2.2544920230110725!3d53.47947686475358!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487bb1dd2d68e415%3A0x3d100bb9c3c15c50!2sThe%20Lawn%20Club!5e0!3m2!1sen!2suk!4v1746363596015!5m2!1sen!2suk" 
         width="500px" 
         height="500px" 
@@ -8,6 +8,14 @@
         allowfullscreen="true" 
         loading="lazy" 
         referrerpolicy="no-referrer-when-downgrade"
+    ></iframe> -->
+    <iframe 
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2374.3836992311303!2d-2.2519171!3d53.47947370000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x487bb1dd2d68e415%3A0x3d100bb9c3c15c50!2sThe%20Lawn%20Club!5e0!3m2!1sen!2suk!4v1755011724882!5m2!1sen!2suk" 
+    width="400" 
+    height="500" 
+    style="border:0;" 
+    loading="lazy" 
+    referrerpolicy="no-referrer-when-downgrade"
     ></iframe>
 </template>
   
--- src/views/gifts.vue
+++ src/views/gifts.vue
@@ -1,14 +1,10 @@
 <template>
     <el-row>
         <el-col :offset="6" :span="12">
-            For typical weddings people might register at retailers or have an gift list, but just want a memorable 
-            wedding celebration with all our friends and family. <br/>
+            We've chosen not to register at a retailer or have an amazon gift list as we want to have a memorable 
+            wedding celebration with all our friends and family. <br/><br/>
             With that being said, we have setup a <i><u>Paypal</u></i> account if anyone wishes to help us with our Honeymoon. If you choose to make a donation 
             please leave a message with a suggestion of where to go, what to do, we'd love the support. <br/><br/>
-
-
-
-
 
             <a href="https://paypal.me/FrancesandAlex2025"><el-button type="primary"><i>Frances and Alex 2025</i></el-button></a>
 
